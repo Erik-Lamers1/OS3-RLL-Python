d6a1d25c159dbb768b63cee543b2231290456cad
SyntheticOxygen <vs.breider@gmail.com>
2020-03-28 21:43:09 +0100

    Started discord client parsing

---
+++ os3_rrl/actions/stub.py
@@ -0,0 +1,38 @@
+from logging import getLogger
+
+from os3_rll.conf import settings
+
+logger = getLogger(__name__)
+
+
+def test_call_int():
+    logger.debug('called: test_call_int()')
+    return "42"
+
+
+def test_call_str():
+    logger.debug('called: test_call_str()')
+    return "The answer to life the universe and everything is fourtytwo (42)\n"
+
+
+def test_call_list():
+    logger.debug('called: test_call_list()')
+    return ['SyntheticOxygen', 'Pandabeer', 'FrietWerk', 'Ronnierups', 'Mr.Vin']
+
+
+def create_challenge(*argv):
+    args = list(*argv)
+    logger.debug('called: stub.create_challenge')
+    logger.debug(f'called with: {args}')
+
+
+def reset_challenge(*argv):
+    args = list(*argv)
+    logger.debug('called: stub.reset_challenge')
+    logger.debug(f'called with: {args}')
+
+
+def complete_challenge(*argv):
+    args = list(*argv)
+    logger.debug('called: stub.complete_challenge')
+    logger.debug(f'called with: {args}')
--- os3_rrl/discord/client.py
+++ os3_rrl/discord/client.py
@@ -2,13 +2,42 @@ import discord
 from logging import getLogger
 
 from os3_rrl.conf import settings
-
+from os3_rrl.actions import stub
 
 logger = getLogger(__name__)
+
+commands = {'get_ranking': stub.test_call_list,
+            'get_active_challenges': stub.test_call_int,
+            'create_challenge': stub.create_challenge,
+            'complete_challenge': stub.complete_challenge,
+            'reset_challenge': stub.reset_challenge
+        }
+
+
+async def on_message(message):
+    logger.debug(f'saw a message: {message}')
+    if message.channel.name == settings.DISCORD_CHANNEL:
+        if message.content.startswith("$"):
+           full_command = message.content[1:]
+           cmd = full_command.split(' ')[0]
+           params = full_command.split(' ')
+           try:
+               try:
+                   res = commands[cmd](params)
+               except ValueError:
+                   logger.error(f'Found a PEBKAC, user provides stupid params: {params}')
+                   res = stub.help()
+           except KeyError:
+               logger.error(f'Unknown command: {cmd}')
 
 
 def discord_client():
     logger.debug('Initializing Discord client')
+
     client = discord.Client()
+
+    logger.debug(f'{client.user is connected to the following guild:\n')
+    logger.debug(f'{guild.name}(id: {guild.id})')
+
     while True:
         client.run(settings.DISCORD_TOKEN)

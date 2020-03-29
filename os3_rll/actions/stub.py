a042421fcf1b571822dc8ff1d2608bce1f59075d
SyntheticOxygen <vs.breider@gmail.com>
2020-03-29 13:33:11 +0200

    Fixed logger prefixes

--- os3_rll/actions/stub.py
+++ os3_rll/actions/stub.py
@@ -8,8 +8,7 @@ logger = getLogger(__name__)
 
 def hello(*argv):
     args = deque(*argv)
-    logger.info('called: hello')
-    logger.info('called with: {}'.format(args))
+    logger.debug('actions.stub.hello: called with: {}'.format(args))
     user = args.popleft()
     res = 'Hi {}\n'.format(user.mention)
     responses = ["How are you doing today? Wait that's retorical, I am a bot I do not care.\n",
@@ -23,40 +22,40 @@ def hello(*argv):
 
 
 def test_call_int(*argv):
-    logger.info('called: test_call_int()')
+    logger.debug('actions.stub.testcalled: test_call_int()')
     return 42
 
 
 def test_call_str(*argv):
-    logger.info('called: test_call_str()')
+    logger.debug('called: test_call_str()')
     return "Don't know, but the answer to life the universe and everything is fourtytwo (42)\n"
 
 
 def test_call_list(*argv):
-    logger.info('called: test_call_list()')
-    return ['SyntheticOxygen', 'Pandabeer', 'FrietWerk', 'Ronnierups', 'Mr.Vin']
+    logger.debug('called: test_call_list()')
+    return ['Toekel', 'SyntheticOxygen', 'Pandabeer', 'FrietWerk', 'Ronnierups', 'Mr.Vin']
 
 
 def create_challenge(*argv):
     args = list(*argv)
-    logger.info('called: stub.create_challenge')
-    logger.info('called with: {}'.format(args))
+    logger.debug('called: stub.create_challenge')
+    logger.debug('called with: {}'.format(args))
 
 
 def reset_challenge(*argv):
     args = list(*argv)
-    logger.info('called: stub.reset_challenge')
-    logger.info('called with: {}'.format(args))
+    logger.debug('called: stub.reset_challenge')
+    logger.debug('called with: {}'.format(args))
 
 
 def complete_challenge(*argv):
     args = list(*argv)
-    logger.info('called: stub.complete_challenge')
-    logger.info('called with: {}'.format(args))
+    logger.debug('called: stub.complete_challenge')
+    logger.debug('called with: {}'.format(args))
 
 
 def stub_help(*argv):
-    logger.info('called: stub.help')
+    logger.debug('called: stub.help')
     responses =['I cannot believe what a stupid FUCK you really are...\n Please do kill -9 $(pgrep yourself)\n',
                 'Is it that difficult to type, do you have butter fingers?\n',
                 'OH MY GOD you are stupid, please rm -rf ./your_life\n',

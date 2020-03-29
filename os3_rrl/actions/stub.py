587ffeb04bb1d33454662822c3dfdf6f7e28c7ba
SyntheticOxygen <vs.breider@gmail.com>
2020-03-29 01:20:41 +0100

    Updated gitignore to exclude vim temp files. fixed bug in stub.py to not have incorrect arguments number.

--- os3_rrl/actions/stub.py
+++ os3_rrl/actions/stub.py
@@ -5,41 +5,41 @@ from os3_rrl.conf import settings
 logger = getLogger(__name__)
 
 
-def test_call_int():
-    logger.debug('called: test_call_int()')
+def test_call_int(*argv):
+    logger.info('called: test_call_int()')
     return 42
 
 
-def test_call_str():
-    logger.debug('called: test_call_str()')
+def test_call_str(*argv):
+    logger.info('called: test_call_str()')
     return "The answer to life the universe and everything is fourtytwo (42)\n"
 
 
-def test_call_list():
-    logger.debug('called: test_call_list()')
+def test_call_list(*argv):
+    logger.info('called: test_call_list()')
     return ['SyntheticOxygen', 'Pandabeer', 'FrietWerk', 'Ronnierups', 'Mr.Vin']
 
 
 def create_challenge(*argv):
     args = list(*argv)
-    logger.debug('called: stub.create_challenge')
-    logger.debug('called with: {}'.format(args))
+    logger.info('called: stub.create_challenge')
+    logger.info('called with: {}'.format(args))
 
 
 def reset_challenge(*argv):
     args = list(*argv)
-    logger.debug('called: stub.reset_challenge')
-    logger.debug('called with: {}'.format(args))
+    logger.info('called: stub.reset_challenge')
+    logger.info('called with: {}'.format(args))
 
 
 def complete_challenge(*argv):
     args = list(*argv)
-    logger.debug('called: stub.complete_challenge')
-    logger.debug('called with: {}'.format(args))
+    logger.info('called: stub.complete_challenge')
+    logger.info('called with: {}'.format(args))
 
 
-def help():
-    logger.debug('called: stub.help')
+def help(*argv):
+    logger.info('called: stub.help')
     responses =['I cannot believe what a stupid FUCK you really are...\n Please do kill -9 $(pgrep yourself)\n',
                 'Is it that difficult to type, do you have butter fingers?\n',
                 'OH MY GOD you are stupid, please rm -rf ./your_life\n',
--- os3_rrl/discord/client.py
+++ os3_rrl/discord/client.py
@@ -3,6 +3,8 @@ from logging import getLogger
 
 from os3_rrl.conf import settings
 from os3_rrl.actions import stub
+from os3_rrl.actions import player
+from os3_rrl.actions import challenge
 
 logger = getLogger(__name__)
 
@@ -10,7 +12,8 @@ commands = {'get_ranking': stub.test_call_list,
             'get_active_challenges': stub.test_call_int,
             'create_challenge': stub.create_challenge,
             'complete_challenge': stub.complete_challenge,
-            'reset_challenge': stub.reset_challenge
+            'reset_challenge': stub.reset_challenge,
+            'help': stub.help
         }
 
 client = discord.Client()
@@ -22,26 +25,29 @@ async def on_ready():
         if guild.name == settings.DISCORD_GUILD:
             break
 
-    logger.debug('{} is connected to the following guild:\n'.format(client.user))
-    logger.debug('{}(id: {})'.format(guild.name, guild.id))
+    logger.info('{} is connected to the following guild:\n'.format(client.user))
+    logger.info('{}(id: {})'.format(guild.name, guild.id))
 
 
 @client.event
 async def on_message(message):
-    logger.debug('saw a message: {}'.format(message))
+    logger.info('saw a message: {}'.format(message)) #
     channel = message.channel
     res = "Ok..."
     if channel.name == settings.DISCORD_CHANNEL:
         if message.content.startswith("$"):
+           logger.info('message.content: {}'.format(message.content))
+           logger.info('message.author: {}{}'.format(message.author.name, message.author.discriminator))
            full_command = message.content.lower()[1:]
            cmd = full_command.split(' ')[0]
-           params = full_command.split(' ')[1:]
+           params = [message.author.name]
+           params.append(full_command.split(' ')[1:])
            try:
                try:
                    res = commands[cmd](params)
                except ValueError:
                    logger.error('Found a PEBKAC, user provides stupid params: {}'.format(params))
-                   res = stub.help()
+                   res = commands['help'](params)
            except KeyError:
                logger.error('Unknown command: {}'.format(cmd))
 
@@ -49,7 +55,7 @@ async def on_message(message):
 
 
 def discord_client():
-    logger.debug('Initializing Discord client')
+    logger.info('Initializing Discord client')
 
     while True:
         client.run(settings.DISCORD_TOKEN)

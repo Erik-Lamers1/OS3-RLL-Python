1c970fbf862e2d3b041867856d719457a6c50a72
SyntheticOxygen <vs.breider@gmail.com>
2020-03-30 20:50:43 +0200

    Refractoring client.py to use the discord.ext.commands instead of using the on_message event for everything

--- os3_rll/discord/client.py
+++ os3_rll/discord/client.py
@@ -24,9 +24,9 @@ commands = {'hi': stub.hello,
             'help': stub.stub_help
             }
 
-bot = discord.Client()
 message_queue = queue.Queue()
 
+bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
@@ -38,38 +38,51 @@ async def on_ready():
     logger.info('bot.on_ready: {}(id: {})'.format(guild.name, guild.id))
 
 
-@bot.event
-async def on_message(message):
-    logger.info('bot.on_message: saw message {} content=<<{}>>'.format(message, message.content))
-    channel = message.channel
+@bot.command()
+async def hi(ctx, *args):
+    logger.debug('bot.command.hi: called with: {} arguments - {}'.format(len(args), ', '.join(args)))
+    res = 'Hi {}\n'.format(ctx.user.mention)
+    responses = ["How are you doing today? Wait that's retorical, I am a bot I do not care.\n",
+                 "I was just looking at your rank. Did you know that you suck at rocket league? I heard some guy SquishyMuffinz is best.\n",
+                 "Please leave me alone. I am randomizing the rankings database to mess with Mr.Vin.\n",
+                 "Due to COVID-19 I've had to reimplement the transport protocol from QUIC to plain UDP to avoid handshakes.\n",
+                 "Please do not bother me. I am looking into this Markov Chain theory. It should be able to give me more human like responses.",
+                 "What are you doing here? LOL, your rank is so low you should practice uninstall.\n"]
+    res += random.choice(responses)
+    await ctx.send(res)
 
-    if channel.name == settings.DISCORD_CHANNEL:
-        if message.content.startswith("$"):
-            logger.info('bot.on_message: message.content = {}'.format(message.content))
-            logger.info('bot.on_message: message.author  = {}#{}'.format(message.author.name, message.author.discriminator))
-            full_command = message.content[1:]
-            cmd = full_command.split(' ')[0]
-            params = [message.author, full_command.split(' ')[1:]]
 
-            try:
-                try:
-                    res = commands[cmd](params)
-                except (TypeError, ValueError) as e:
-                    logger.error('bot.on_message: Found a PEBKAC, user provides stupid params: {}\n'.format(params) +
-                                 'This resulted in the following error:\n{}\n'.format(str(e))
-                                )
-                    res = commands['help'](params)
-            except KeyError:
-                logger.error('bot.on_message: unknown command {}'.format(cmd))
-
-            if res is None:
-                res = "Ok..."
-
-            if type(res) is dict:
-                await channel.send(res['content'], embed=res['embed'])
-            else:
-                await channel.send(res)
-
+#@bot.event
+#async def on_message(message):
+#    logger.info('bot.on_message: saw message {} content=<<{}>>'.format(message, message.content))
+#    channel = message.channel
+#
+#    if channel.name == settings.DISCORD_CHANNEL:
+#        if message.content.startswith("$"):
+#            logger.info('bot.on_message: message.content = {}'.format(message.content))
+#            logger.info('bot.on_message: message.author  = {}#{}'.format(message.author.name, message.author.discriminator))
+#            full_command = message.content[1:]
+#            cmd = full_command.split(' ')[0]
+#            params = [message.author, full_command.split(' ')[1:]]
+#
+#            try:
+#                try:
+#                    res = commands[cmd](params)
+#                except (TypeError, ValueError) as e:
+#                    logger.error('bot.on_message: Found a PEBKAC, user provides stupid params: {}\n'.format(params) +
+#                                 'This resulted in the following error:\n{}\n'.format(str(e))
+#                                )
+#                    res = commands['help'](params)
+#            except KeyError:
+#                logger.error('bot.on_message: unknown command {}'.format(cmd))
+#
+#            if res is None:
+#                res = "Ok..."
+#
+#            if type(res) is dict:
+#                await channel.send(res['content'], embed=res['embed'])
+#            else:
+#                await channel.send(res)
 
 async def post():
     logger.debug('bot.post: started background task')

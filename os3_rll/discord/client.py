28fabc8970655647de965a8cd12b40914a59e553
SyntheticOxygen <vs.breider@gmail.com>
2020-04-03 18:44:25 +0200

    trying to provide default help instead of overloaded crap

--- os3_rll/discord/client.py
+++ os3_rll/discord/client.py
@@ -122,7 +122,7 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.CheckFailure):
         await ctx.send("Whooops, you are not allowed to do this. Ask an RLL Admin.")
     else:
-        await ctx.send(bot.help())
+        await ctx.send(bot.help_command())
 
 
 async def post():

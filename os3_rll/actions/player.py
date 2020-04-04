d8b632839b9047e8398226ac59d3c29f750dd6ca
Erik Lamers <lamerse95@gmail.com>
2020-04-04 11:34:46 +0200

    Implement player stats function

--- os3_rll/actions/player.py
+++ os3_rll/actions/player.py
@@ -1,6 +1,8 @@
 from logging import getLogger
 
 from os3_rll.models.db import Database, DBException
+from os3_rll.models.player import Player
+from os3_rll.operations.player import get_all_player_ids_ordered, get_average_goals_per_challenge
 
 logger = getLogger(__name__)
 
@@ -21,4 +23,39 @@ def get_player_ranking():
         for row in rows:
             # Fill the dict with discord => rank
             players[row[0]] = row[1]
+    return players
+
+
+def get_player_stats():
+    """
+    Get all the player stats
+    returns dict of dicts: ->
+        { player_id: {
+            name                    -> str gamertag,
+            discord                 -> str discord,
+            rank                    -> int rank,
+            wins                    -> int wins,
+            losses                  -> int losses
+            is_challenged
+            avg_goals_per_challenge -> int goals
+            },
+            ...
+        }
+    """
+    players = {}
+    logger.info('Retrieving player stats')
+    ids = get_all_player_ids_ordered()
+    for player in ids:
+        # Get the basic info
+        with Player(player) as p:
+            players[p.id] = {
+                "name": p.gamertag,
+                "discord": p.discord,
+                "rank": p.rank,
+                "wins": p.wins,
+                "losses": p.losses,
+                "is_challenged": p.challenged
+            }
+        # Now get the average goals per challenge
+        players[player]["avg_goals_per_challenge"] = get_average_goals_per_challenge(player)
     return players
--- os3_rll/operations/player.py
+++ os3_rll/operations/player.py
@@ -1,8 +1,8 @@
 from threading import Lock
 from logging import getLogger
 
-from os3_rll.models.player import Player
-from os3_rll.models.db import Database
+from os3_rll.models.player import Player, PlayerException
+from os3_rll.models.db import Database, DBException
 
 logger = getLogger(__name__)
 
@@ -51,3 +51,52 @@ def update_rank_of_player_cascading(player1, player2):
                     _update_rank_of_player_cascading(other_player, other_rank+1)
 
         _update_rank_of_player_cascading(player1, player2)
+
+
+def get_all_player_ids_ordered(order_by='rank'):
+    """
+    Get a list of all player ids ordered by column
+
+    param str order_by: Which column to order by
+    return list: The player ids
+    """
+    ids = []
+    with Database() as db:
+        db.execute_prepared_statement('SELECT id FROM users ORDER BY %s', (order_by,))
+        if db.rowcount == 0:
+            raise DBException('No users returned')
+        rows = db.fetchall()
+        for row in rows:
+            ids.append(row[0])
+    return ids
+
+
+def get_average_goals_per_challenge(player):
+    """
+    Gets the average goals per challenge for a player
+
+    param int player: The player id to get the average goals for
+    return int: The average goals per challenge
+    """
+    logger.debug('Calculating average goals per challenge for player with id {}'.format(player))
+    with Database as db:
+        # Average goals as p1
+        db.execute_prepared_statement(
+            'SELECT AVG(p1_score) FROM challenges WHERE p1=%s AND WINNER IS NOT NULL', (player,)
+        )
+        if db.rowcount != 1:
+            logger.warning('Player with id {} has never challenged someone, setting average challenger score to 0')
+            avg_challenger_score = 0
+        else:
+            avg_challenger_score = int(db.fetchone()[0])
+        # Average goals as p2
+        db.execute_prepared_statement(
+            'SELECT AVG(p2_score) FROM challenges WHERE p2=%s AND WINNER IS NOT NULL', (player,)
+        )
+        if db.rowcount != 1:
+            logger.warning('Player with id {} has never been challenged, setting average challenged score to 0')
+            avg_challenged_score = 0
+        else:
+            avg_challenged_score = int(db.fetchone()[0])
+    # Return the average of the two numbers
+    return int(avg_challenger_score + avg_challenged_score / 2)

167ec1162e91eb31b02f525bdb04f6e68b1c9ccd
Erik Lamers <lamerse95@gmail.com>
2020-04-08 19:51:54 +0200

    First working test

---
+++ os3_rll/tests/log/test_log.py
@@ -0,0 +1,11 @@
+import logging
+
+from os3_rll.log.log import setup_console_logging
+from os3_rll.tests import TestCase
+from os3_rll.conf import settings
+
+
+class TestLog(TestCase):
+    def test_setup_console_logging_sets_up_INFO_logging_by_default(self):
+        setup_console_logging()
+        self.assertEqual(settings.LOGGING['handlers']['console']['level'], logging.INFO)

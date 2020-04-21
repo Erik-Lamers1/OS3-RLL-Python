ae4c46aec9f30e660f181034934f9c9a43e754cf
Erik Lamers <lamerse95@gmail.com>
2020-04-21 14:55:31 +0200

    Finish tests for RLL main file

--- os3_rll/tests/rocket_league_ladder/test_main.py
+++ os3_rll/tests/rocket_league_ladder/test_main.py
@@ -1,4 +1,4 @@
-from logging import INFO
+from logging import INFO, DEBUG
 
 from os3_rll.tests import OS3RLLTestCase
 from os3_rll.rocket_league_ladder import main
@@ -8,6 +8,7 @@ class TestRLLMain(OS3RLLTestCase):
     def setUp(self):
         self.client = self.set_up_patch("os3_rll.rocket_league_ladder.discord_client")
         self.logging = self.set_up_patch("os3_rll.rocket_league_ladder.setup_console_logging")
+        self.show_version = self.set_up_patch("os3_rll.rocket_league_ladder.show_version")
 
     def test_main_calls_discord_client(self):
         main()
@@ -15,4 +16,23 @@ class TestRLLMain(OS3RLLTestCase):
 
     def test_main_calls_setup_console_logging(self):
         main()
-        self.logging.assert_called_once_with(verbosity=20)
+        self.logging.assert_called_once_with(verbosity=INFO)
+
+    def test_main_calls_setup_console_logging_verbose(self):
+        main(["--verbose"])
+        self.logging.assert_called_once_with(verbosity=DEBUG)
+
+    def test_main_calls_show_version_when_version_is_passed(self):
+        with self.assertRaises(SystemExit):
+            main(["--version"])
+        self.show_version.assert_called_once_with()
+
+    def test_main_exits_0_when_version_is_passed(self):
+        with self.assertRaises(SystemExit) as e:
+            main(["--version"])
+        self.assertEqual(e.exception.code, 0)
+
+    def test_main_does_not_call_discord_client_when_version_is_passed(self):
+        with self.assertRaises(SystemExit):
+            main(["--version"])
+        self.client.assert_not_called()
